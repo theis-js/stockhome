501f94ab5de2c5985e009d611a30cdf24de3b1be
Theis Gaedigk <theis.gaedigk@icloud.com>
2026-05-29 22:18:02 +0200

    improved frontend routing

--- frontend/src/routes/app/_hiddenLayout/app-settings.tsx
+++ frontend/src/routes/app/_hiddenLayout/app-settings.tsx
@@ -1,7 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { isAuthenticated } from "../../../utils/auth";
 import { Settings } from "../../../pages/Settings";
 
 export const Route = createFileRoute("/app/_hiddenLayout/app-settings")({
+  beforeLoad: async () => {
+    if (!(await isAuthenticated())) {
+      throw redirect({
+        to: "/login",
+      });
+    }
+  },
   component: RouteComponent,
 });
 
--- frontend/src/routes/app/_hiddenLayout/storages.tsx
+++ frontend/src/routes/app/_hiddenLayout/storages.tsx
@@ -1,7 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { isAuthenticated } from "../../../utils/auth";
 import { Storages } from "../../../pages/Storages";
 
 export const Route = createFileRoute("/app/_hiddenLayout/storages")({
+  beforeLoad: async () => {
+    if (!(await isAuthenticated())) {
+      throw redirect({
+        to: "/login",
+      });
+    }
+  },
   component: RouteComponent,
 });
 
