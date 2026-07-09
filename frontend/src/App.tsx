0d4a048d16fb456ffc263ac05efdafea19ea91de
Theis Gaedigk <theis.gaedigk@icloud.com>
2026-07-09 13:21:10 +0200

    refatcor: remove old tostafiy npm package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import "./App.css";
-import { ToastContainer } from "react-toastify";
 import { routeTree } from "./routeTree.gen";
+import { NotFound } from "./components/NotFound.tsx";
 
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, defaultNotFoundComponent: NotFound });
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -12,23 +12,7 @@ declare module "@tanstack/react-router" {
 }
 
 function App() {
-  return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
